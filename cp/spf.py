08124fe6df1416461c7cf13b064871a27f7d0a7e
Sumit Singh <76198431+abcsumits@users.noreply.github.com>
2023-09-25 13:28:41 +0530

    Update spf.py

--- cp/spf.py
+++ cp/spf.py
@@ -1,9 +1,9 @@
-spf=[0]*(n-1)#0 index refer to 2  so on
+spf=[0]*(n+1)
 for i in range(2,n+1):
-    if spf[i-2]==0:
-        spf[i-2]=i
+    if spf[i]==0:
+        spf[i]=i
         c=i+i
     while c<=n:
-        if spf[c-2]==0:
-            spf[c-2]=i
+        if spf[c]==0:
+            spf[c]=i
         c+=i
