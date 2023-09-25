0d4e001d8f8e19829ad206ff5c34fae023a9eb13
Sumit Singh <76198431+abcsumits@users.noreply.github.com>
2023-09-25 13:30:37 +0530

    Update spf.py

--- cp/spf.py
+++ cp/spf.py
@@ -3,7 +3,8 @@ for i in range(2,n+1):
     if spf[i]==0:
         spf[i]=i
         c=i+i
-    while c<=n:
-        if spf[c]==0:
-            spf[c]=i
-        c+=i
+        while c<=n:
+            if spf[c]==0:
+                spf[c]=i
+            c+=i
+
