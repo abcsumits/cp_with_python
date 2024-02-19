4af04be98a1ea1bd98f4c3b3bb31ed717bf193e1
Sumit Singh <76198431+abcsumits@users.noreply.github.com>
2024-02-20 03:04:27 +0530

    Create k-th_ancestor_binary_lifting.py

---
+++ cp/k-th_ancestor_binary_lifting.py
@@ -0,0 +1,44 @@
+class TreeAncestor:
+
+    def __init__(self, n, gp,root):#gp means graph matrix, n number of nodes,root node  
+        bit=1
+        t=n
+        while t:
+            t//=2
+            bit+=1
+        bit_ancestor=[] 
+        for i in range(n):
+            bit_ancestor.append([-1]*bit)
+        stack=[[root,0]] 
+        while stack:
+            curr=stack[-1] 
+            node=curr[0]
+            upcomming=curr[1] 
+            if upcomming==0 and node!=root:
+                c=stack[-2][0]
+                for i in range(1,bit):
+                    if c==-1:
+                        break
+                    bit_ancestor[node][i]=c 
+                    c=bit_ancestor[c][i]
+            if upcomming==len(gp[node]):
+                stack.pop() 
+            else:
+                stack[-1][1]+=1
+                stack.append([gp[node][upcomming],0]) 
+        self.ancestor=bit_ancestor
+        self.bit=bit
+
+
+
+        
+
+    def getKthAncestor(self, node, k) : # returns -1 for invalid queries
+        t=1
+        for i in range(1,self.bit):
+            if t&k:
+                node=self.ancestor[node][i]
+            if node==-1:
+                return -1
+            t<<=1
+        return node
