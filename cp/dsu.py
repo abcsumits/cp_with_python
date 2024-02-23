5b1b38eaabfd5189f3c6904aa055b3817e00f489
Sumit Singh <76198431+abcsumits@users.noreply.github.com>
2024-02-23 15:07:19 +0530

    Update dsu.py

--- cp/dsu.py
+++ cp/dsu.py
@@ -2,11 +2,15 @@ class dsu:
     def __init__(self,n):
         self.size=[1]*(n+1)
         self.parent=[x for x in range(n+1)]
-    def find(self,x):
-        if x==self.parent[x]:
-            return x
-        self.parent[x]=dsu.find(self,self.parent[x])
-        return self.parent[x]
+   def find(self,x):
+        t=x
+        while self.parent[t]!=t:
+            t=self.parent[t]
+        while self.parent[x]!=x:
+            f=x
+            x=self.parent[x]
+            self.parent[f]=t
+        return x
     def union(self,u,v):
         x=dsu.find(self,u)
         y=dsu.find(self,v)
